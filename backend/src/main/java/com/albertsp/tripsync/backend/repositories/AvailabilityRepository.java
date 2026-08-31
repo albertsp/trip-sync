e84fbe3fd6604bb39613d220f73c5f7a6ac700fe
albertsp <albertsanchez98@gmail.com>
2026-08-31 11:50:03 +0200

    feat: add methods findByParticipantTipId in AvailabilityRepository and FindTripId in ParticipantRepository

--- backend/src/main/java/com/albertsp/tripsync/backend/repositories/AvailabilityRepository.java
+++ backend/src/main/java/com/albertsp/tripsync/backend/repositories/AvailabilityRepository.java
@@ -3,5 +3,10 @@ package com.albertsp.tripsync.backend.repositories;
 import com.albertsp.tripsync.backend.domain.Availability;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface AvailabilityRepository extends JpaRepository<Availability,Long> {
+
+    List<Availability> findByParticipantTripId(UUID tripId);
 }
