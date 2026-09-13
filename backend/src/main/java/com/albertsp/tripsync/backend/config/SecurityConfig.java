12c98b80aafd45295e785c3652166ce95fb7bf75
albertsp <albertsanchez98@gmail.com>
2026-09-13 14:39:33 +0200

    feat: add Google OAuth2 login and secure trip creation endpoint

---
+++ backend/src/main/java/com/albertsp/tripsync/backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.albertsp.tripsync.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/trips").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login((oauth2) ->oauth2.defaultSuccessUrl("http://localhost:5173/",true))
+                .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).ignoringRequestMatchers("/trips/*/participants"));
+        return http.build();
+    }
+}
